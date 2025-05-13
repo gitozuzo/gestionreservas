673d88bb1b89f334db972577aafb60239bcdd1d1
Jorge Manchado Leal <jmanchadolea@uoc.edu>
2025-05-13 22:58:29 +0200

    se habilita las cors

--- backend/src/main/java/com/gestion/reservas/config/CorsConfig.java
+++ backend/src/main/java/com/gestion/reservas/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200")); // o "*" para pruebas
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
